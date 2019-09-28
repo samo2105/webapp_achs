991a988a5c521c26e417f688dc77fcee4f12971b
Samuel <samuel.cea95@gmail.com>
2019-09-27 23:23:15 -0300

    Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,8 +4,5 @@ class Article < ApplicationRecord
 
   has_many :coments
 
-  def self.find_articles(user)
-    articles = Article.where(tag_id: user.tags).order(created_at: :desc)
-    return articles
-  end
+
 end
